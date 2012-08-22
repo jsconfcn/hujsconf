dfdf2e1a78f6c0d51ce845f8a6afc718ea4b77d5
Glenn Block <gblock@microsoft.com>
2012-08-23 01:10:38 +0800

    adding images and local speakers

--- schedule.js
+++ schedule.js
@@ -1,7 +1,7 @@
 exports.talks = [
 	{
-		"topic" : "Stuff about things",
-		"abstract" : "A long description of this",
+		"topic" : "",
+		"abstract" : "",
 		"date" : new Date("2012-09-14T14:30:00Z"),
 		"duration" : 30,
 		"speaker" : {
@@ -9,14 +9,13 @@ exports.talks = [
 			"url" : "http://github.com/substack",
 			"image" : "substack.jpg",
 			"blurb" : "Creator of <a href='https://github.com/substack/fleet' target='_blank'>fleet</a> and 279 other repositories on Github.",
-			"bio:" : "a longer bio about this person",
 			"tags" : "Node, International",
 			"should_display" : "true"
 		}
 	},
 	{
-		"topic" : "Stuff about things",
-		"abstract" : "A long description of this",
+		"topic" : "",
+		"abstract" : "",
 		"date" : new Date("2012-09-14T09:30:00Z"),
 		"duration" : 30,
 		"speaker" : {
@@ -24,14 +23,13 @@ exports.talks = [
 			"url" : "http://github.com/creationix",
 			"image" : "creationix.jpg",
 			"blurb" : "Creator of <a href='https://howtonode.org' target='_blank'>howtonode.org</a>.",
-			"bio:" : "A bio has not yet been submitted.",
 			"tags" : "Node, International",
 			"should_display" : "true"
 		}
 	},
 	{
-		"topic" : "Stuff about things",
-		"abstract" : "A long description of this",
+		"topic" : "",
+		"abstract" : "",
 		"date" : new Date("2012-09-14T13:30:00Z"),
 		"duration" : 30,
 		"speaker" : {
@@ -39,14 +37,13 @@ exports.talks = [
 			"url" : "http://github.com/indexzero",
 			"image" : "indexzero.jpg",
 			"blurb" : "Co-founder of <a href='https://nodejitsu.com' target='_blank'>nodejitsu</a>.",
-			"bio:" : "A bio has not yet been submitted.",
 			"tags" : "Node, International",
 			"should_display" : "true"
 		}
 	},
 	{
-		"topic" : "Stuff about things",
-		"abstract" : "A long description of this",
+		"topic" : "",
+		"abstract" : "",
 		"date" : new Date("2012-09-15T11:30:00Z"),
 		"duration" : 30,
 		"speaker" : {
@@ -54,14 +51,13 @@ exports.talks = [
 			"url" : "http://github.com/SaraJo",
 			"image" : "sarajchipps.jpg",
 			"blurb" : "CTO @<a href='http://levoleague.com'>levoleague</a>. Made <a href='http://twitter.com/girldevelopit'>@girldevelopit</a> and <a href='http://twitter.com/perfect_basics'>@perfect_basics</a>",
-			"bio:" : "A bio has not yet been submitted.",
-			"tags" : "ClientJS, International",
+			"tags" : "JavaScript, International",
 			"should_display" : "true"
 		}
 	},
 	{
-		"topic" : "Stuff about things",
-		"abstract" : "A long description of this",
+		"topic" : "",
+		"abstract" : "",
 		"date" : new Date("2012-09-15T11:30:00Z"),
 		"duration" : 30,
 		"speaker" : {
@@ -69,14 +65,13 @@ exports.talks = [
 			"url" : "http://github.com/glennblock",
 			"image" : "gblock.jpg",
 			"blurb" : "Senior Program Manager @<a href='http://www.microsoft.com'>microsoft</a>. Brought node.js to <a href='http://twitter.com/windowsazure'>@windowsazure</a>. All around supporter of OSS",
-			"bio:" : "A bio has not yet been submitted.",
 			"tags" : "Node, Azure, International",
 			"should_display" : "true"
 		}
 	},
 	{
-		"topic" : "Stuff about things",
-		"abstract" : "A long description of this",
+		"topic" : "",
+		"abstract" : "",
 		"date" : new Date("2012-09-15T11:30:00Z"),
 		"duration" : 30,
 		"speaker" : {
@@ -84,14 +79,13 @@ exports.talks = [
 			"url" : "http://github.com/guille",
 			"image" : "rauchg.png",
 			"blurb" : "CTO and Cofounder @<a href='http://learnboost.com'>Learnboost</a>. Author of <a href='http://socket.io'>socket.io</a>.",
-			"bio:" : "A bio has not yet been submitted.",
 			"tags" : "Node, Socket.io, International",
 			"should_display" : "true"
 		}
 	},
 	{
-		"topic" : "Stuff about things",
-		"abstract" : "A long description of this",
+		"topic" : "",
+		"abstract" : "",
 		"date" : new Date("2012-09-15T11:30:00Z"),
 		"duration" : 30,
 		"speaker" : {
@@ -99,13 +93,12 @@ exports.talks = [
 			"url" : "http://github.com/atmos",
 			"image" : "atmos.jpg",
 			"blurb" : "Developer at <a href='http://github.com'>github</a>. Author of <a href='http://hubot.github.com'>hubot</a>, <a href='http://atomos.org/cinderella'> cinderella</a> and <a href='http://atomos.org/camo'>camo</camo>",
-			"bio:" : "A bio has not yet been submitted.",
 			"tags" : "Node, International",
 			"should_display" : "true"
 		}
 	},
 	{
-		"topic" : "Stuff about things",
+		"topic" : "",
 		"abstract" : "A long description of this",
 		"date" : new Date("2012-09-15T11:30:00Z"),
 		"duration" : 30,
@@ -114,14 +107,13 @@ exports.talks = [
 			"url" : "http://github.com/vmg",
 			"image" : "vmg.jpg",
 			"blurb" : "Developer at <a href='http://github.com'>github</a>.",
-			"bio:" : "A bio has not yet been submitted.",
 			"tags" : "Node, International",
 			"should_display" : "true"
 		}
 	},
 	{
-		"topic" : "Stuff about things",
-		"abstract" : "A long description of this",
+		"topic" : "",
+		"abstract" : "",
 		"date" : new Date("2012-09-15T11:30:00Z"),
 		"duration" : 30,
 		"speaker" : {
@@ -129,12 +121,108 @@ exports.talks = [
 			"url" : "http://github.com/stevesanderson",
 			"image" : "stevensanderson.jpg",
 			"blurb" : "Developer at <a href='http://microsoft.com'>microsoft</a>. Creator of <a href='http://knockoutjs.com'/>knockout.js</a>",
-			"bio:" : "A bio has not yet been submitted.",
-			"tags" : "International, ClientJS",
+			"tags" : "International, JavaScript",
 			"should_display" : "true"
 		}
 	},
-
+	{
+		"topic" : "",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T11:30:00Z"),
+		"duration" : 30,
+		"speaker" : {
+			"name" : "Troy Howard", 
+			"url" : "http://github.com/thoward",
+			"image" : "thoward37.jpg",
+			"blurb" : "Developer at <a href='http://appfog.com'>AppFog</a>. Organizer of NodePDX and HuJS",
+			"tags" : "International, NodeJS",
+			"should_display" : "true"
+		}
+	},
+	{
+		"topic" : "",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T11:30:00Z"),
+		"duration" : 30,
+		"speaker" : {
+			"name" : "Leslie Hawthorn", 
+			"url" : "http://github.com/lhawthorn",
+			"image" : "lhawthorn.jpg",
+			"blurb" : "Community manager@<a href='http://redhat.com'>Redhat</a>, formerly googler, published speaker and author",
+			"tags" : "International, OSS",
+			"should_display" : "true"
+		}
+	},
+	{
+		"topic" : "",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T11:30:00Z"),
+		"duration" : 30,
+		"speaker" : {
+			"name" : "Python发烧友/苏千", 
+			"url" : "http://github.com/fengmk2",
+			"image" : "fengmk2.png",
+			"blurb" : "Developer@<a href='http://taobao.com'>Taobao</a>. A crazy web guy",
+			"tags" : "Local, Node",
+			"should_display" : "true"
+		}
+	},
+	{
+		"topic" : "",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T11:30:00Z"),
+		"duration" : 30,
+		"speaker" : {
+			"name" : "寒冬winter/程劭非", 
+			"url" : "http://github.com/wintercn",
+			"image" : "寒冬winter.jpg",
+			"blurb" : "Developer@<a href='http://snda.com'>SNDA</a>.",
+			"tags" : "Local, Node",
+			"should_display" : "true"
+		}
+	},	
+	{
+		"topic" : "",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T11:30:00Z"),
+		"duration" : 30,
+		"speaker" : {
+			"name" : "老赵/赵劼", 
+			"url" : "http://github.com/JeffreyZhao",
+			"image" : "老赵.png",
+			"blurb" : "Editor @<a href='http://infoq.com'>InfoQ</a>.",
+			"tags" : "Local, JavaScript",
+			"should_display" : "true"
+		}
+	},	
+	{
+		"topic" : "",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T11:30:00Z"),
+		"duration" : 30,
+		"speaker" : {
+			"name" : "玉伯也叫射雕/王保平", 
+			"url" : "http://github.com/lifesinger",
+			"image" : "王保平.jpg",
+			"blurb" : "Developer @<a href='http://alipay.com'>Alipay</a>.",
+			"tags" : "Local, JavaScript, Node",
+			"should_display" : "true"
+		}
+	},	
+	{
+		"topic" : "",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T11:30:00Z"),
+		"duration" : 30,
+		"speaker" : {
+			"name" : "小问/甘超阳", 
+			"url" : "http://github.com/iwillwen",
+			"image" : "小问.png",
+			"blurb" : "Founder @C61 Labs.",
+			"tags" : "Local, Node, NoSQL",
+			"should_display" : "true"
+		}
+	},				
 	{
 		"topic" : "Hackthon",
 		"abstract" : "A day long hackathon at KIC. This session is only open to those who registered for the full event.",
