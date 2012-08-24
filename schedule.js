5d1100208f5369ca446baf175e70814218493249
Troy Howard <thoward37@gmail.com>
2012-08-23 19:56:22 -0700

    Arranged schedule

--- schedule.js
+++ schedule.js
@@ -1,9 +1,24 @@
 exports.talks = [
+	/* international speakers */
+	{
+		"topic" : "Keynote: Building International Communities",
+		"abstract" : "", 
+		"date" : new Date("2012-09-14T08:50:00Z"),
+		"duration" : 20,
+		"speaker" : {
+			"name" : "Troy Howard", 
+			"url" : "http://github.com/thoward",
+			"image" : "thoward37.jpg",
+			"blurb" : "Developer at <a href='http://appfog.com'>AppFog</a>. Organizer of NodePDX and HuJS",
+			"tags" : "International, NodeJS",
+			"should_display" : "true"
+		}
+	},
 	{
 		"topic" : "Node.js TBD",
 		"abstract" : "",
-		"date" : new Date("2012-09-14T14:30:00Z"),
-		"duration" : 30,
+		"date" : new Date("2012-09-14T09:15:00Z"),
+		"duration" : 40,
 		"speaker" : {
 			"name" : "James Halliday", 
 			"url" : "http://github.com/substack",
@@ -14,10 +29,24 @@ exports.talks = [
 		}
 	},
 	{
+		"topic" : "Hubot.js, the build and deployment engine for github backed by node",
+		"abstract" : "",
+		"date" : new Date("2012-09-14T11:30:00Z"),
+		"duration" : 30,
+		"speaker" : {
+			"name" : "Vincent Marti", 
+			"url" : "http://github.com/vmg",
+			"image" : "vmg.jpg",
+			"blurb" : "Developer at <a href='http://github.com'>github</a>.",
+			"tags" : "Node, International",
+			"should_display" : "true"
+		}
+	},
+	{
 		"topic" : "Node.js TBD",
 		"abstract" : "",
-		"date" : new Date("2012-09-14T09:30:00Z"),
-		"duration" : 30,
+		"date" : new Date("2012-09-14T13:00:00Z"),
+		"duration" : 40,
 		"speaker" : {
 			"name" : "Tim Caswell", 
 			"url" : "http://github.com/creationix",
@@ -30,8 +59,8 @@ exports.talks = [
 	{
 		"topic" : "Node.js TBD",
 		"abstract" : "",
-		"date" : new Date("2012-09-14T13:30:00Z"),
-		"duration" : 30,
+		"date" : new Date("2012-09-14T16:00:00Z"),
+		"duration" : 40,
 		"speaker" : {
 			"name" : "Charlie Robbins", 
 			"url" : "http://github.com/indexzero",
@@ -41,11 +70,26 @@ exports.talks = [
 			"should_display" : "true"
 		}
 	},
+	/* Day 2 */
+	{
+		"topic" : "0 to 60 mobile apps using Azure Mobile and Knockout.js",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T09:00:00Z"),
+		"duration" : 40,
+		"speaker" : {
+			"name" : "Steven Sanderson", 
+			"url" : "http://github.com/stevesanderson",
+			"image" : "stevensanderson.jpg",
+			"blurb" : "Developer at <a href='http://microsoft.com'>microsoft</a>. Creator of <a href='http://knockoutjs.com'/>knockout.js</a>",
+			"tags" : "International, JavaScript",
+			"should_display" : "true"
+		}
+	},
 	{
 		"topic" : "JavaScript Client TBD",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
+		"date" : new Date("2012-09-15T10:40:00Z"),
+		"duration" : 40,
 		"speaker" : {
 			"name" : "Sara Chipps", 
 			"url" : "http://github.com/SaraJo",
@@ -56,9 +100,23 @@ exports.talks = [
 		}
 	},
 	{
+		"topic" : "Engine.io",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T13:00:00Z"),
+		"duration" : 40,
+		"speaker" : {
+			"name" : "Guillermo Rauch", 
+			"url" : "http://github.com/guille",
+			"image" : "rauchg.png",
+			"blurb" : "CTO and Cofounder @<a href='http://learnboost.com'>Learnboost</a>. Author of <a href='http://socket.io'>socket.io</a>.",
+			"tags" : "Node, Socket.io, International",
+			"should_display" : "true"
+		}
+	},
+	{
 		"topic" : "node.js and Microsoft",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
+		"date" : new Date("2012-09-15T14:20:00Z"),
 		"duration" : 30,
 		"speaker" : {
 			"name" : "Glenn Block", 
@@ -70,24 +128,10 @@ exports.talks = [
 		}
 	},
 	{
-		"topic" : "Engine.io",
-		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
-		"speaker" : {
-			"name" : "Guillermo Roach", 
-			"url" : "http://github.com/guille",
-			"image" : "rauchg.png",
-			"blurb" : "CTO and Cofounder @<a href='http://learnboost.com'>Learnboost</a>. Author of <a href='http://socket.io'>socket.io</a>.",
-			"tags" : "Node, Socket.io, International",
-			"should_display" : "true"
-		}
-	},
-	{
 		"topic" : "Real world experiences using ember.js",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
+		"date" : new Date("2012-09-15T16:00:00Z"),
+		"duration" : 40,
 		"speaker" : {
 			"name" : "Corey Donahue", 
 			"url" : "http://github.com/atmos",
@@ -97,80 +141,12 @@ exports.talks = [
 			"should_display" : "true"
 		}
 	},
-	{
-		"topic" : "Hubot.js, the build and deployment engine for github backed by node",
-		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
-		"speaker" : {
-			"name" : "Vincent Marti", 
-			"url" : "http://github.com/vmg",
-			"image" : "vmg.jpg",
-			"blurb" : "Developer at <a href='http://github.com'>github</a>.",
-			"tags" : "Node, International",
-			"should_display" : "true"
-		}
-	},
-	{
-		"topic" : "0 to 60 mobile apps using Azure Mobile and Knockout.js",
-		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
-		"speaker" : {
-			"name" : "Steven Sanderson", 
-			"url" : "http://github.com/stevesanderson",
-			"image" : "stevensanderson.jpg",
-			"blurb" : "Developer at <a href='http://microsoft.com'>microsoft</a>. Creator of <a href='http://knockoutjs.com'/>knockout.js</a>",
-			"tags" : "International, JavaScript",
-			"should_display" : "true"
-		}
-	},
-	{
-		"topic" : "Opener to HuJS",
-		"abstract" : "", 
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
-		"speaker" : {
-			"name" : "Troy Howard", 
-			"url" : "http://github.com/thoward",
-			"image" : "thoward37.jpg",
-			"blurb" : "Developer at <a href='http://appfog.com'>AppFog</a>. Organizer of NodePDX and HuJS",
-			"tags" : "International, NodeJS",
-			"should_display" : "true"
-		}
-	},
-	{
-		"topic" : "Building OSS commmunities",
-		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
-		"speaker" : {
-			"name" : "Leslie Hawthorn", 
-			"url" : "http://github.com/lhawthorn",
-			"image" : "lhawthorn.jpg",
-			"blurb" : "Community manager@<a href='http://redhat.com'>Redhat</a>, formerly googler, published speaker and author",
-			"tags" : "International, OSS",
-			"should_display" : "true"
-		}
-	},
-	{
-		"topic" : "在Node.js上踩过的坑",
-		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
-		"speaker" : {
-			"name" : "Python发烧友/苏千", 
-			"url" : "http://github.com/fengmk2",
-			"image" : "fengmk2.png",
-			"blurb" : "Developer@<a href='http://taobao.com'>Taobao</a>. A crazy web guy",
-			"tags" : "China, Node",
-			"should_display" : "true"
-		}
-	},
+
+	/* local speakers */
 	{
 		"topic" : "Ecmascript 5 in action",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
+		"date" : new Date("2012-09-14T10:00:00Z"),
 		"duration" : 30,
 		"speaker" : {
 			"name" : "寒冬winter/程劭非", 
@@ -182,9 +158,23 @@ exports.talks = [
 		}
 	},	
 	{
+		"topic" : "在Node.js上踩过的坑",
+		"abstract" : "",
+		"date" : new Date("2012-09-14T10:45:00Z"),
+		"duration" : 40,
+		"speaker" : {
+			"name" : "Python发烧友/苏千", 
+			"url" : "http://github.com/fengmk2",
+			"image" : "fengmk2.png",
+			"blurb" : "Developer@<a href='http://taobao.com'>Taobao</a>. A crazy web guy",
+			"tags" : "China, Node",
+			"should_display" : "true"
+		}
+	},
+	{
 		"topic" : "Wind.js的调试与排错支持",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
+		"date" : new Date("2012-09-14T13:45:00Z"),
 		"duration" : 30,
 		"speaker" : {
 			"name" : "老赵/赵劼", 
@@ -198,7 +188,7 @@ exports.talks = [
 	{
 		"topic" : "SeaJS - 更自然的代码组织方式",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
+		"date" : new Date("2012-09-14T14:20:00Z"),
 		"duration" : 30,
 		"speaker" : {
 			"name" : "玉伯也叫射雕/王保平", 
@@ -212,7 +202,7 @@ exports.talks = [
 	{
 		"topic" : "让服务器和客户端牵起手来",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
+		"date" : new Date("2012-09-14T16:45:00Z"),
 		"duration" : 30,
 		"speaker" : {
 			"name" : "小问/甘超阳", 
@@ -226,8 +216,8 @@ exports.talks = [
 	{
 		"topic" : "基于Node.js的分布式MySQL代理服务",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
+		"date" : new Date("2012-09-14T17:20:00Z"),
+		"duration" : 40,
 		"speaker" : {
 			"name" : "王达心", 
 			"url" : "http://github.com/freedaxin",
@@ -237,11 +227,12 @@ exports.talks = [
 			"should_display" : "true"
 		}
 	},
+	/* Day 2 */
 	{
 		"topic" : "基于Chromium和node.js的桌面应用Runtime",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
+		"date" : new Date("2012-09-15T09:45:00Z"),
+		"duration" : 40,
 		"speaker" : {
 			"name" : "小型笨蛋/赵成", 
 			"url" : "http://github.com/zcbenz",
@@ -254,8 +245,8 @@ exports.talks = [
 	{
 		"topic" : "node.js游戏服务器开发",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
+		"date" : new Date("2012-09-15T11:25:00Z"),
+		"duration" : 35,
 		"speaker" : {
 			"name" : "圈圈套圈圈", 
 			"url" : "",
@@ -266,23 +257,9 @@ exports.talks = [
 		}
 	},				
 	{
-		"topic" : "rrestjs框架介绍及轻博客www.wujb.net应用",
-		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
-		"speaker" : {
-			"name" : "snoopy/吴中骅", 
-			"url" : "",
-			"image" : "snoopy-吴中骅.jpg",
-			"blurb" : "Developer@<a href = 'http://www.163.com'>netease</a>",
-			"tags" : "China, Node, REST",
-			"should_display" : "true"
-		}
-	},
-	{
 		"topic" : "Node性能跟踪与调优",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
+		"date" : new Date("2012-09-15T13:45:00Z"),
 		"duration" : 30,
 		"speaker" : {
 			"name" : "朋春/张轩丞", 
@@ -296,8 +273,8 @@ exports.talks = [
 	{
 		"topic" : "Node.js TBD",
 		"abstract" : "",
-		"date" : new Date("2012-09-15T11:30:00Z"),
-		"duration" : 30,
+		"date" : new Date("2012-09-15T16:45:00Z"),
+		"duration" : 40,
 		"speaker" : {
 			"name" : "JeffSu", 
 			"url" : "https://github.com/jeffsu",
@@ -306,7 +283,82 @@ exports.talks = [
 			"tags" : "China, Node",
 			"should_display" : "true"
 		}
-	},	
+	},
+	{
+		"topic" : "rrestjs框架介绍及轻博客www.wujb.net应用",
+		"abstract" : "",
+		"date" : new Date("2012-09-15T17:30:00Z"),
+		"duration" : 30,
+		"speaker" : {
+			"name" : "snoopy/吴中骅", 
+			"url" : "",
+			"image" : "snoopy-吴中骅.jpg",
+			"blurb" : "Developer@<a href = 'http://www.163.com'>netease</a>",
+			"tags" : "China, Node, REST",
+			"should_display" : "true"
+		}
+	},
+	/* non speaking schedule items */
+	{
+		"topic" : "Morning Announcements",
+		"abstract" : "Some brief announcements about the conference.",
+		"date" : new Date("2012-09-15T08:50:00Z"),
+		"duration" : 10
+	},
+	{
+		"topic" : "Coffee Break",
+		"abstract" : "Enjoy coffee or tea, chat and make some new friends!",
+		"date" : new Date("2012-09-14T10:30:00Z"),
+		"duration" : 15
+	},
+	{
+		"topic" : "Lunch Break",
+		"abstract" : "Catered lunch at KIC",
+		"date" : new Date("2012-09-14T12:00:00Z"),
+		"duration" : 60
+	},
+	{
+		"topic" : "Panel Discussion",
+		"abstract" : "All of our speakers will join us on stage to have a panel discussion, including Q/A.",
+		"date" : new Date("2012-09-14T13:00:00Z"),
+		"duration" : 45
+	},
+	{
+		"topic" : "After Party Announcements",
+		"abstract" : "A brief announcement about the evening events that will going on after the conference.",
+		"date" : new Date("2012-09-14T18:00:00Z"),
+		"duration" : 10
+	},
+	{
+		"topic" : "Coffee Break",
+		"abstract" : "Enjoy coffee or tea, chat and make some new friends!",
+		"date" : new Date("2012-09-15T15:45:00Z"),
+		"duration" : 15
+	},
+	{
+		"topic" : "Coffee Break",
+		"abstract" : "Enjoy coffee or tea, chat and make some new friends!",
+		"date" : new Date("2012-09-14T15:45:00Z"),
+		"duration" : 15
+	},
+	{
+		"topic" : "Lunch Break",
+		"abstract" : "Catered lunch at KIC",
+		"date" : new Date("2012-09-15-0T12:00:00Z"),
+		"duration" : 60
+	},
+	{
+		"topic" : "Panel Discussion",
+		"abstract" : "All of our speakers will join us on stage to have a panel discussion, including Q/A.",
+		"date" : new Date("2012-09-15T13:00:00Z"),
+		"duration" : 45
+	},
+	{
+		"topic" : "After Party Announcements",
+		"abstract" : "A brief announcement about the evening events that will going on after the conference.",
+		"date" : new Date("2012-09-15T18:00:00Z"),
+		"duration" : 10
+	},
 	{
 		"topic" : "Hackthon",
 		"abstract" : "A day long hackathon at KIC. This session is only open to those who registered for the full event.",
