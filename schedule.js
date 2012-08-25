8ebe49ddfa6dff93d7b5012804a72e543a7f4f72
Glenn Block <gblock@microsoft.com>
2012-08-25 22:39:00 +0800

    making sort algorithm go by either name or by pinyin key for Chinese names

--- schedule.js
+++ schedule.js
@@ -41,7 +41,8 @@ exports.talks = [
 			"image" : "vmg.jpg",
 			"blurb" : "Fancy pants, Systems engineer@<a href='http://github.com'>github</a>.",
 			"tags" : "Node.js, International",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "en"
 		}
 	},
 	{
@@ -55,7 +56,8 @@ exports.talks = [
 			"image" : "creationix.jpg",
 			"blurb" : "Tinkerer and Mentor (<a href='http://howtonode.org' target='_blank'>howtonode.org</a>, <a href='http://nodebits.org' target='_blank'>nodebits.org</a>)",
 			"tags" : "Node.js, International",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "en"
 		}
 	},
 	{
@@ -69,7 +71,8 @@ exports.talks = [
 			"image" : "indexzero.jpg",
 			"blurb" : "Co-founder of <a href='http://www.nodejitsu.com' target='_blank'>nodejitsu</a>.",
 			"tags" : "Node.js, International",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "en"
 		}
 	},
 	/* Day 2 */
@@ -84,7 +87,8 @@ exports.talks = [
 			"image" : "stevensanderson.jpg",
 			"blurb" : "Developer at <a href='http://www.microsoft.com'>microsoft</a>. Creator of <a href='http://knockoutjs.com'/>knockout.js</a>",
 			"tags" : "International, JavaScript",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "en"
 		}
 	},
 	{
@@ -98,7 +102,8 @@ exports.talks = [
 			"image" : "sarajchipps.jpg",
 			"blurb" : "CTO @<a href='http://levoleague.com'>levoleague</a>. Made <a href='http://twitter.com/girldevelopit'>@girldevelopit</a> and <a href='http://twitter.com/perfect_basics'>@perfect_basics</a>",
 			"tags" : "JavaScript, International",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "en"
 		}
 	},
 	{
@@ -112,7 +117,8 @@ exports.talks = [
 			"image" : "rauchg.png",
 			"blurb" : "CTO and Cofounder @<a href='http://learnboost.com'>Learnboost</a>. Author of <a href='http://socket.io'>socket.io</a>.",
 			"tags" : "Node, Socket.io, International",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "en"
 		}
 	},
 	{
@@ -126,7 +132,8 @@ exports.talks = [
 			"image" : "gblock.jpg",
 			"blurb" : "Senior Program Manager @<a href='http://www.microsoft.com'>microsoft</a>. Brought node.js to <a href='http://twitter.com/windowsazure'>@windowsazure</a>. All around supporter of OSS",
 			"tags" : "Node.js, Scalability, International",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "en"
 		}
 	},
 	{
@@ -140,7 +147,8 @@ exports.talks = [
 			"image" : "atmos.jpg",
 			"blurb" : "Developer at <a href='http://github.com'>github</a>. Author of <a href='http://hubot.github.com'>hubot</a>, <a href='http://atomos.org/cinderella'> cinderella</a> and <a href='http://atomos.org/camo'>camo</camo>",
 			"tags" : "Node.js, International",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "en"
 		}
 	},
 
@@ -152,11 +160,13 @@ exports.talks = [
 		"duration" : 30,
 		"speaker" : {
 			"name" : "寒冬winter/程劭非", 
+			"key": "Wintercn",
 			"url" : "http://github.com/wintercn",
 			"image" : "wintercn.jpg",
 			"blurb" : "Developer @<a href='http://snda.com'>SNDA</a>.",
 			"tags" : "China, Node.js",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},	
 	{
@@ -165,12 +175,14 @@ exports.talks = [
 		"date" : new Date("2012-09-14T10:45:00Z"),
 		"duration" : 40,
 		"speaker" : {
-			"name" : "Python发烧友/苏千", 
+			"name" : "Python发烧友/苏千",
+			"key" : "Fengmk2", 
 			"url" : "http://github.com/fengmk2",
 			"image" : "fengmk2.png",
 			"blurb" : "Developer @<a href='http://www.taobao.com'>Taobao</a>. A crazy web guy",
 			"tags" : "China, Node.js",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},
 	{
@@ -179,12 +191,14 @@ exports.talks = [
 		"date" : new Date("2012-09-14T13:45:00Z"),
 		"duration" : 30,
 		"speaker" : {
-			"name" : "老赵/赵劼", 
+			"name" : "老赵/赵劼",
+			"key" : "Jeffzhao", 
 			"url" : "http://github.com/JeffreyZhao",
 			"image" : "jeffzhao.png",
 			"blurb" : "Editor @<a href='http://www.infoq.com'>InfoQ</a>.",
 			"tags" : "China, JavaScript",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},	
 	{
@@ -193,12 +207,14 @@ exports.talks = [
 		"date" : new Date("2012-09-14T14:20:00Z"),
 		"duration" : 30,
 		"speaker" : {
-			"name" : "玉伯也叫射雕/王保平", 
+			"name" : "玉伯也叫射雕/王保平",
+			"key" : "Lifesinger", 
 			"url" : "http://github.com/lifesinger",
 			"image" : "lifesinger.jpg",
 			"blurb" : "Developer @<a href='http://alipay.com'>Alipay</a>.",
 			"tags" : "China, JavaScript, Node.js",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},	
 	{
@@ -207,12 +223,14 @@ exports.talks = [
 		"date" : new Date("2012-09-14T16:45:00Z"),
 		"duration" : 30,
 		"speaker" : {
-			"name" : "小问/甘超阳", 
+			"name" : "小问/甘超阳",
+			"key" : "Iwillwen",
 			"url" : "http://github.com/iwillwen",
 			"image" : "iwillwen.png",
 			"blurb" : "Author of <a href='http://github.com/iwillwen/webjs' target='_blank'>webjs</a> and <a href='http://iwillwen.github.com/NanoDB/' target='_blank'>NanoDB</a>",
 			"tags" : "China, Node.js, Database",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},
 	{
@@ -221,12 +239,14 @@ exports.talks = [
 		"date" : new Date("2012-09-14T17:20:00Z"),
 		"duration" : 40,
 		"speaker" : {
-			"name" : "王达心", 
+			"name" : "王达心",
+			"key" : "Freedaxin", 
 			"url" : "http://github.com/freedaxin",
 			"image" : "freedaxin.png",
 			"blurb" : "Development engineer at <a href='http://www.weibo.com'>Sina Weibo</a>",
 			"tags" : "China, Node.js, Database",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},
 	/* Day 2 */
@@ -236,12 +256,14 @@ exports.talks = [
 		"date" : new Date("2012-09-15T09:45:00Z"),
 		"duration" : 40,
 		"speaker" : {
-			"name" : "小型笨蛋/赵成", 
+			"name" : "小型笨蛋/赵成",
+			"key" : "Zcbenz", 
 			"url" : "http://github.com/zcbenz",
 			"image" : "zcbenz.jpg",
 			"blurb" : "Intern at <a href='http://www.intel.com'>Intel</a>",
 			"tags" : "China, Node.js",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},				
 	{
@@ -250,12 +272,14 @@ exports.talks = [
 		"date" : new Date("2012-09-15T11:25:00Z"),
 		"duration" : 35,
 		"speaker" : {
-			"name" : "谢骋超/圈圈套圈圈", 
-			"url" : "http://www.cdblp.cn/author/%E8%B0%A2%E9%AA%8B%E8%B6%85",
+			"name" : "谢骋超/圈圈套圈圈",
+			"key" : "Xiechengchao", 
+			"url" : "",
 			"image" : "xiechengchao.jpg",
 			"blurb" : "Research Institute senior program specialist @<a href = 'http://www.163.com'>netease</a>",
 			"tags" : "China, Node.js, Database",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},				
 	{
@@ -265,11 +289,13 @@ exports.talks = [
 		"duration" : 30,
 		"speaker" : {
 			"name" : "朋春/张轩丞", 
+			"key" : "Aleafs",
 			"url" : "http://github.com/aleafs",
 			"image" : "aleafs.jpg",
 			"blurb" : "Developer@<a href = 'http://www.taobao.com'>tabao</a>",
 			"tags" : "China, Node.js, Performance, Scalability",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},	
 	{
@@ -283,7 +309,8 @@ exports.talks = [
 			"image" : "jeffsu.jpg",
 			"blurb" : "Staff Software Engineer @<a href = 'http://www.factual.com'>Factual</a> author of <a href='http://github.com/jeffsu/mochiscript'>Mochiscript</a>",
 			"tags" : "China, Node.js",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "en"
 		}
 	},
 	{
@@ -293,11 +320,13 @@ exports.talks = [
 		"duration" : 30,
 		"speaker" : {
 			"name" : "snoopy/吴中骅", 
+			"key" : "Snoopy",
 			"url" : "http://github.com/DoubleSpout",
 			"image" : "doublespout.jpg",
 			"blurb" : "Developer @<a href = 'http://www.163.com'>netease</a>",
 			"tags" : "China, Node.js, REST",
-			"should_display" : "true"
+			"should_display" : "true",
+			"language" : "cn"
 		}
 	},
 	/* non speaking schedule items */
